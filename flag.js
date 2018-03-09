9ae1a9accd75cb4006c7db536c6f5911f164f2da
Craig Orcutt <craigaorcutt@gmail.com>
2018-03-09 14:31:05 -0600

    flag completed

---
+++ flag.js
@@ -0,0 +1,19 @@
+#!/usr/local/bin env node
+
+const chalk = require('chalk');
+const flag = () =>{
+console.log(chalk.bgBlue(chalk.white('* * * * * * *'), chalk.bgRed('                                     ') ));
+console.log(chalk.bgBlue(chalk.white(' * * * * * * '), chalk.bgWhite('                                     ') ));
+console.log(chalk.bgBlue(chalk.white('* * * * * * *'), chalk.bgRed('                                     ') ));
+console.log(chalk.bgBlue(chalk.white(' * * * * * * '), chalk.bgWhite('                                     ') ));
+console.log(chalk.bgBlue(chalk.white('* * * * * * *'), chalk.bgRed('                                     ') ));
+console.log(chalk.bgBlue(chalk.bgWhite('                                                   ') ));
+console.log(chalk.bgBlue(chalk.bgRed('                                                   ') ));
+console.log(chalk.bgBlue(chalk.bgWhite('                                                   ') ));
+console.log(chalk.bgBlue(chalk.bgRed('                                                   ') ));
+console.log(chalk.bgBlue(chalk.bgWhite('                                                   ') ));
+console.log(chalk.bgBlue(chalk.bgRed('                                                   ') ));
+
+}
+
+flag();
