a57ff8d2bc946ca41105322368ec27f25bb3f23d
Craig Orcutt <craigaorcutt@gmail.com>
2018-03-09 13:47:15 -0600

    got beginnings of flag set up

---
+++ index.js
@@ -0,0 +1,9 @@
+const chalk = require('chalk');
+
+console.log(chalk.bgBlue(chalk.white('* * * * *'), chalk.bgRed('                ') ));
+console.log(chalk.bgBlue(chalk.white('* * * * *'), chalk.bgWhite('                ') ));
+console.log(chalk.bgBlue(chalk.white('* * * * *'), chalk.bgRed('                ') ));
+console.log(chalk.bgBlue(chalk.white('* * * * *'), chalk.bgWhite('                ') ));
+console.log(chalk.bgBlue(chalk.white('* * * * *'), chalk.bgRed('                ') ));
+console.log(chalk.bgBlue(chalk.white('* * * * *'), chalk.bgWhite('                ') ));
+console.log(chalk.bgBlue(chalk.white('* * * * *'), chalk.bgRed('                ') ));
